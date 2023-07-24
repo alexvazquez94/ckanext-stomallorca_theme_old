36a19766badb9c1779b50a94d2aad3caaef1138b
alexvazquez94 <alejandrovazquezllabres@gmail.com>
2023-07-24 08:16:28 +0200

    Commit inicial

---
+++ ckanext/stomallorca_theme/assets/script.js
@@ -0,0 +1,15 @@
+ckan.module("stomallorca_theme-module", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {},
+  };
+});
+
+/* $(".navbar .nav-link").on("click", function(){
+  $(".navbar").find(".active").removeClass("active");
+  $(this).addClass("active");
+}); */
